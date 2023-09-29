fe14de7447dcce5ad604e7e76b0c9dfe9f28ce4e
Jhojan Tobón <jhojantobonm@gmail.com>
2023-09-29 17:52:00 -0500

    adding the plugins into the rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,29 @@
+import html from '@rollup/plugin-html';
+import css from 'rollup-plugin-css-only';
+import copy from 'rollup-plugin-copy';
+import image from '@rollup/plugin-image';
+
+
 export default {
   input: "src/index.js",
   output:{
     file: "dist/bundle.js",
     format: "es",
     sourcemap: true,
-  }
+  },
+  plugins:[
+    copy({
+      targets:[{
+        src: 'src/assets/**/*',
+        dest: 'dest/assets'
+        
+      }]
+    }),
+    css({
+      output: 'bundle.css'
+    }),
+    html(),
+    image()
+  ]
+
 }
